21f6d94a629273568203be9c04b6a56a5c2c8f94
boniface pereira <boniface.pereira@softage.net>
2020-08-26 01:03:59 +0530

    this is my second commit

--- ytdl.py
+++ ytdl.py
@@ -16,41 +16,55 @@ import requests
 arguments = sys.argv
 search_term = arguments[1]
 
+
+def search_youtube(term):
+    url = "https://www.youtube.com/results?search_query=" + parse.quote(search_term)
+    response = requests.get("https://www.youtube.com/results?search_query=ducati+monster+821+top+speed", headers={
+        'user-agent': 'Opera/9.80 (iPhone; Opera Mini/8.0.0/34.2336; U; en) Presto/2.8.119 Version/11.10'
+    })
+
+    # todo: get the json data, and get the complete video list,
+    # get out the video seconds, description, channel name, title, and video id, payment required flag
+    # for 2020-08-26
+    html = str(response.content, 'utf-8')
+
+    # find and get video id from html string.
+    pos = html.find('watch?v=')
+    pos += 8
+    key = html[pos:pos + 30]
+
+    posQ = key.find('"')
+    youtubeId = key[0: posQ]
+
+    return youtubeId
+
+
 # https://www.youtube.com/results?search_query=ducati+monster+821+top+speed
 
-url = "https://www.youtube.com/results?search_query=" + parse.quote(search_term)
+def download_youtube(youtubeId):
+    # exercise 1:
+    # pos = html.find('watch?v=') .
+    # specify that v= is the start and " is the end.
+    # get the substring
+    import youtube_dl
+    options = {
+        'format': 'bestvideo/best',
+        # 'postprocessors': [{
+        #     'key': 'FFmpegExtractAudio',
+        # 'preferredcodec': 'mp3',
+        # 'preferredquality': '192',
+        # }],
+    }
+    yt = youtube_dl.YoutubeDL(options)
+    yt.download(['https://www.youtube.com/watch?v=' + str(youtubeId)])
 
-# response = requests.get("https://www.youtube.com/results?search_query=ducati+monster+821+top+speed")
-response = urllib.request.urlopen(url).read()
-html = str(response, 'utf-8')
 
-pos = html.find('watch?v=')
-pos += 8
-key = html[pos:pos + 30]
+def init_function():
+    print('Searching for ' + str(search_term))
+    youtubeId = search_youtube(search_term)
+    print('Found first result on youtube, id is ' + str(youtubeId))
+    download_youtube(youtubeId)
+    print('The file is now downloaded')
 
-posQ = key.find('"')
-youtubeId = key[0: posQ]
 
-# exercise 1:
-# pos = html.find('watch?v=') .
-# specify that v= is the start and " is the end.
-# get the substring
-
-print('The youtube id is ' + str(youtubeId))
-
-import youtube_dl
-
-options = {
-    'format': 'bestvideo/best',
-    # 'postprocessors': [{
-    #     'key': 'FFmpegExtractAudio',
-    # 'preferredcodec': 'mp3',
-    # 'preferredquality': '192',
-    # }],
-}
-
-yt = youtube_dl.YoutubeDL(options)
-yt.download(['https://www.youtube.com/watch?v=' + str(youtubeId)])
-
-print('The file is now downloaded')
-
+init_function()
